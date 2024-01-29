db25acf7909717a063afafcb5e94203e8022216e
nickbytee <nikhilkaushik2228@gmail.com>
2024-01-29 15:48:28 +0530

    base react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lets Get Started</h1>
+        <ExpenseItem>
+          <h3>Food Rs 10</h3>
+          <h3>Petrol Rs 100</h3>
+          <h3>Movies Rs 200</h3>
+        </ExpenseItem>
       </header>
     </div>
   );
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return(
+    <div>
+    <h2>Expenses</h2>
+    <h5>Food Rs 10</h5>
+    <h5>Petrol Rs 100</h5>
+    <h5>Movies Rs 200</h5>
+    </div>)
+}
+
+export default ExpenseItem
