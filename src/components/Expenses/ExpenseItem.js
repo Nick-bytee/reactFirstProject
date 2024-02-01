a77210ad2ed0891d2c237d80ba9d0c8088b65303
nickbytee <nikhilkaushik2228@gmail.com>
2024-02-01 11:15:37 +0530

    added useSate function

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,34 @@
 import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
+import React, { useState } from "react";
+
+const clickHandler = () => {
+  console.log("item Deleted");
+};
 
 const ExpenseItem = (expense) => {
+  const [title, setTitle] = useState(expense.title);
+  const [amount, setAmount] = useState(expense.amount);
+
+  const amountClickHandler = () => {
+    setAmount(100);
+  };
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={expense.date}></ExpenseDate>
       <ExpenseDetails
-        title={expense.title}
+        title={title}
         location={expense.location}
-        amount={expense.amount}
+        amount={amount}
       ></ExpenseDetails>
+      <button onClick={amountClickHandler}>change expense</button>
+      <button onClick={clickHandler}>Update</button>
     </div>
   );
 };
